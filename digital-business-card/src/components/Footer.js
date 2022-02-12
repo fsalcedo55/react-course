3afff249a13186172504e42f1e07019c72e43db4
Fernando Salcedo <fsalcedo55@hotmail.com>
2022-02-12 10:30:00 -0500

    Digital business card styling

--- digital-business-card/src/components/Footer.js
+++ digital-business-card/src/components/Footer.js
@@ -2,8 +2,11 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div>
-      <h1>Footer section</h1>
+    <div className="footer">
+      <img src="" alt="bird-logo" />
+      <img src="" alt="f-logo" />
+      <img src="" alt="camera-logo" />
+      <img src="" alt="cat-logo" />
     </div>
   );
 }
--- digital-business-card/src/components/Info.js
+++ digital-business-card/src/components/Info.js
@@ -4,13 +4,13 @@ import photo from "../images/IMG_7063.jpg";
 export default function Info() {
   return (
     <div className="info">
-      <img src={photo} width="300" alt="profile" />
+      <img src={photo} width="317" alt="profile" />
       <h2 className="name">Fernando Salcedo</h2>
       <h4 className="occupation">Frontend Developer</h4>
       <p className="email">fsalcedo55@hotmail.com</p>
-      <div>
-        <button>Email</button>
-        <button>LinkedIn</button>
+      <div className="buttons-div">
+        <button className="button">Email</button>
+        <button className="button">LinkedIn</button>
       </div>
     </div>
   );
